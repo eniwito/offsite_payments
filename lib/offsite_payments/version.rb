d9d12f0f1320befa1dc98c74c0e4548c760139a8
alexanderpower <aleksandr.power88@gmail.com>
2015-12-10 15:56:14 +0500

    module Yandexkassa created

--- lib/offsite_payments/version.rb
+++ lib/offsite_payments/version.rb
@@ -1,3 +1,15 @@
 module OffsitePayments
-  VERSION = "2.2.0"
+  # TODO исправить
+  #
+  # Bundler could not find compatible versions for gem "offsite_payments":
+  #      In Gemfile:
+  #             spree (= 2.3.10) ruby depends on
+  #               spree_core (= 2.3.10) ruby depends on
+  #                 activemerchant (~> 1.44.1) ruby depends on
+  #                   offsite_payments (~> 2.0.0) ruby
+  #
+  #             offsite_payments (>= 0) ruby
+  #
+  # пока оставил так
+  VERSION = "2.0.1"
 end
