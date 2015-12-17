c8009e539dde5ff340e303afc664d2c2498b66b8
alexanderpower <aleksandr.power88@gmail.com>
2015-12-17 19:02:59 +0500

    changed parameters

--- lib/offsite_payments/integrations/yandexkassa.rb
+++ lib/offsite_payments/integrations/yandexkassa.rb
@@ -52,6 +52,7 @@ module OffsitePayments #:nodoc:
         mapping :scid, 'scid'
         mapping :shopId, 'shopId'
         mapping :shopArticleId, 'shopArticleId'
+        mapping :paymentType , 'paymentType'
       end
 
       class Notification < OffsitePayments::Notification
@@ -61,37 +62,37 @@ module OffsitePayments #:nodoc:
         end
 
         def complete?
-          params['_raw_action'] == 'paymentAviso'
+          params['action'] == 'paymentAviso'
         end
 
         def item_id
-          params['_raw_orderNumber']
+          params['orderNumber']
         end
 
         def transaction_id
-          params['_raw_invoiceId']
+          params['invoiceId']
         end
 
         # When was this payment received by the client.
         def received_at
-          params['_raw_orderCreatedDatetime']
+          params['orderCreatedDatetime']
         end
 
         def currency
-          params['_raw_orderSumCurrencyPaycash']
+          params['orderSumCurrencyPaycash']
         end
 
         def payer_email
-          params['_raw_cps_email']
+          params['cps_email']
         end
 
         # the money amount we received in X.2 decimal.
         def gross
-          params['_raw_orderSumAmount'].to_f
+          params['orderSumAmount'].to_f
         end
 
         def customer_id
-          params['_raw_customerNumber']
+          params['customerNumber']
         end
 
         def set_response(code)
@@ -108,7 +109,7 @@ module OffsitePayments #:nodoc:
         end
 
         def status
-          case params['_raw_action']
+          case params['action']
             when 'checkOrder'
               'pending'
             when 'paymentAviso'
@@ -119,8 +120,8 @@ module OffsitePayments #:nodoc:
         end
 
         def response
-          shop_id = params['_raw_shopId']
-          method = params['_raw_action']
+          shop_id = params['shopId']
+          method = params['action']
           dt = Time.now.iso8601
           "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
               "<#{method}Response performedDatetime=\"#{dt}\" code=\"#{@response_code}\"" +
@@ -147,18 +148,18 @@ module OffsitePayments #:nodoc:
         #
 
         def acknowledge(authcode = nil)
-          string = [params['_raw_action'],
-                    params['_raw_orderSumAmount'],
-                    params['_raw_orderSumCurrencyPaycash'],
-                    params['_raw_orderSumBankPaycash'],
-                    params['_raw_shopId'],
-                    params['_raw_invoiceId'],
-                    params['_raw_customerNumber'],
+          string = [params['action'],
+                    params['orderSumAmount'],
+                    params['orderSumCurrencyPaycash'],
+                    params['orderSumBankPaycash'],
+                    params['shopId'],
+                    params['invoiceId'],
+                    params['customerNumber'],
                     authcode
           ].join(';')
 
           digest = Digest::MD5.hexdigest(string)
-          res = params['_raw_md5'] == digest.upcase
+          res = params['md5'] == digest.upcase
           if res
             @response_code = '0'
           else
@@ -166,17 +167,22 @@ module OffsitePayments #:nodoc:
           end
         end
 
-        private
-
-        # Take the posted data and move the relevant data into a hash
-        def parse(post)
-          @raw = post.to_s
-          for line in @raw.split('&')
-            key, value = *line.scan(%r{^([A-Za-z0-9_.-]+)\=(.*)$}).flatten
-            # to divide raw values from other
-            params['_raw_' + key] = CGI.unescape(value.to_s) if key.present?
-          end
-        end
+        # private
+        #
+        # # Take the posted data and move the relevant data into a hash
+        # def parse(post)
+        #   # TODO for PKCS#7 encryption
+        #   hash = Hash.from_xml(post)
+        #   self.params = hash.first[1].deep_dup
+        #   self.params['action'] = hash.first[0][/(.*)Request/,1]
+        #
+        #   # @raw = post.to_s
+        #   # for line in @raw.split('&')
+        #   #   key, value = *line.scan(%r{^([A-Za-z0-9_.-]+)\=(.*)$}).flatten
+        #   #   # to divide raw values from other
+        #   #   params['' + key] = CGI.unescape(value.to_s) if key.present?
+        #   # end
+        # end
       end
     end
   end
